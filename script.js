c2241868d4363c8e52a1a4feaee4bf6bdab7b8b2
achintyajha <achintyakjha@gmail.com>
2021-11-01 00:44:48 +0530

    team

--- script.js
+++ script.js
@@ -24,11 +24,11 @@
 //   }
 // })();
 
-const toggle = () => {
+function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
-};
+}
